a9c500e181f652acb23c372f82b5e836c6a7108a
jxx <jxx@jxxdeMacBook-Pro.local>
2021-01-23 19:54:02 +0800

    add

---
+++ src/main/java/dataStructure/LinkedNode.java
@@ -0,0 +1,28 @@
+package dataStructure;
+
+/**
+ * @className: LinkedNode
+ * @description: 链表
+ * @author: jxx
+ * @date: 2021/1/23 6:21 PM
+ **/
+public class LinkedNode {
+    public int val;
+    public LinkedNode next;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public LinkedNode getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedNode next) {
+        this.next = next;
+    }
+}
